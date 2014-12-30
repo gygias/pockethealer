da315f780f240adb5661689dcdb0b7d6c54c2b22
gygias <gygias@users.noreply.github.com>
2014-12-29 20:22:35 -0600

    Initial Commit

---
+++ heal drudge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  heal drudge
+//
+//  Created by david on 12/29/14.
+//  Copyright (c) 2014 Combobulated Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ heal drudge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  heal drudge
+//
+//  Created by david on 12/29/14.
+//  Copyright (c) 2014 Combobulated Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
