cbe7886a6622d2d7a8b942da844e025bbde2dbe6
gygias <gygias@users.noreply.github.com>
2015-02-19 15:30:01 -0800

    new files

---
+++ heal drudge/PassThroughView.h
@@ -0,0 +1,13 @@
+//
+//  PassThroughView.h
+//  heal drudge
+//
+//  Created by david on 2/19/15.
+//  Copyright (c) 2015 Combobulated Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PassThroughView : UIView
+
+@end
