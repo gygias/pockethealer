c0fd7afa487ec9082cf74651bcfed45c680a0432
gygias <gygias@users.noreply.github.com>
2015-02-19 17:55:12 -0800

    ...

---
+++ heal drudge/PowerWordBarrierEffect.h
@@ -0,0 +1,13 @@
+//
+//  PowerWordBarrierEffect.h
+//  heal drudge
+//
+//  Created by david on 2/19/15.
+//  Copyright (c) 2015 Combobulated Software. All rights reserved.
+//
+
+#import "Effect.h"
+
+@interface PowerWordBarrierEffect : Effect
+
+@end
