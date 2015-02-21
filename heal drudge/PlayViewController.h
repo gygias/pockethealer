d7ab35effa79d8244d2b4abeda0e68af44c4906a
gygias <gygias@users.noreply.github.com>
2015-02-20 17:26:56 -0800

    modularizing 'play' view, breaks landscape mode

---
+++ heal drudge/PlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayViewController.h
+//  heal drudge
+//
+//  Created by david on 2/20/15.
+//  Copyright (c) 2015 Combobulated Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayViewController : UIViewController
+
+@end
